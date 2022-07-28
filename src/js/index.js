de73392ee32434f68463c18a797c4686caab0d74
KirillKorotkiy <104353705+KirillKorotkiy@users.noreply.github.com>
2022-07-28 14:36:25 +0300

    fixed

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 
 import './menu';
 
-
 const toolBartRef = document.querySelector('.theme-switch__toggle');
 const bodyRef = document.querySelector('body');
 
@@ -10,29 +9,33 @@ toolBartRef.addEventListener('change', switchTheme);
 function switchOnDark() {
   bodyRef.classList.add('dark-theme');
   bodyRef.classList.remove('light-theme');
-  const themeName = bodyRef.className;
-  localStorage.setItem('theme-name', themeName);
-  toolBartRef.checked = true;
+  setLocalStorage()
 }
 
 function switchOnLight() {
   bodyRef.classList.add('light-theme');
   bodyRef.classList.remove('dark-theme');
-  const themeName = bodyRef.className;
-  localStorage.setItem('theme-name', themeName);
-  toolBartRef.checked = false;
+  setLocalStorage()
 }
 
 function switchTheme(event) {
     event.currentTarget.checked ? switchOnDark():switchOnLight()
 }
 
+function setLocalStorage(){
+  const themeName = bodyRef.className;
+  localStorage.setItem('theme-name', themeName);
+}
+
 function setDefault (){
+  
     const getCurrentTheme = localStorage.getItem('theme-name');
     if (getCurrentTheme === 'dark-theme') {
         switchOnDark();
+        toolBartRef.checked = true;
         return;
 }   switchOnLight();
+    toolBartRef.checked = false;
 }
 
 setDefault()
